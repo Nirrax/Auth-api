4675a277a42710f834778cbc0cfe51c6ff494272
Nirrax <Nirraxx@gmail.com>
2023-12-12 18:59:06 +0100

    Implementing signUp function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -12,19 +13,30 @@ export class AuthService {
     const hash = await argon.hash(dto.password);
 
     //save new user to the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        passwordHash: hash,
-        firstName: dto.firstName,
-        lastName: dto.lastName,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          passwordHash: hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
 
-    //delete passwordHash from response
-    delete user.passwordHash;
+      //delete passwordHash from response
+      delete user.passwordHash;
 
-    return user;
+      return user;
+    } catch (error) {
+      //catch prisma error
+      if (error instanceof PrismaClientKnownRequestError) {
+        //duplicate record
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signIn() {
