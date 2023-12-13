6258cfc2dfe0a2a619a27060d388200b66728e72
Nirrax <Nirraxx@gmail.com>
2023-12-13 04:01:42 +0100

    Changing response type in signToken function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -62,7 +62,10 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  signToken(userId: number, email: string): Promise<string> {
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
@@ -70,9 +73,13 @@ export class AuthService {
 
     const secret = this.config.get('JWT_SECRET');
 
-    return this.jwt.signAsync(payload, {
+    const token = await this.jwt.signAsync(payload, {
       expiresIn: '15m',
       secret: secret,
     });
+
+    return {
+      access_token: token,
+    };
   }
 }
