3121dd342b3919254e9be2782a2c90ca3045a0eb
Nirrax <Nirraxx@gmail.com>
2024-01-28 23:51:30 +0100

    Implementing classify function

--- src/classification/classification.service.ts
+++ src/classification/classification.service.ts
@@ -1,7 +1,8 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class ClassificationService {
@@ -28,18 +29,50 @@ export class ClassificationService {
   }
 
   async classify(user: User, base64Data: string) {
-    const data = base64Data;
+    // send request to microservice
+    const response = await this.sendRequest(base64Data);
 
-    let response;
+    // save classification to the database
+    const classification = await this.SaveClassificationToDb(user, response);
+
+    return classification;
+  }
+
+  async sendRequest(base64Data: string) {
     try {
-      response = await this.httpService
-        .post('http://127.0.0.1:8000', data)
+      const response = await this.httpService
+        .post('http://127.0.0.1:8000', base64Data)
         .toPromise();
+
+      return response;
     } catch (error) {
       console.error(error.message);
     }
+  }
 
-    console.log(response.data);
-    return response.data;
+  async SaveClassificationToDb(user: any, response: any) {
+    try {
+      const classification = await this.prisma.classification.create({
+        data: {
+          //fileName: response.filename,
+          fileName: 'output',
+          genre: response.data.genre,
+          genreDistribution: response.data.genreDistribution,
+          genreSequence: response.data.genreSequence,
+          userId: user.id,
+        },
+      });
+
+      return classification;
+    } catch (error) {
+      //catch prisma error
+      if (error instanceof PrismaClientKnownRequestError) {
+        //duplicate record
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 }
