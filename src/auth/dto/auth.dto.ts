47225c1c3964b3a463ba9b66bffd0cdfe8840e18
Nirrax <Nirraxx@gmail.com>
2024-02-14 19:01:30 +0100

    Creating dto for classification module, and fixing auth module's dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,28 +1,34 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsDefined, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignUpDto {
+  @IsDefined()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   lastName: string;
 }
 
 export class SignInDto {
+  @IsDefined()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   password: string;
