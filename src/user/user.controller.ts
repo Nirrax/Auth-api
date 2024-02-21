394a10b71e4206e8a7738ea1192b22fc524d4d31
Nirrax <Nirraxx@gmail.com>
2024-02-21 05:50:45 +0100

    Implementing CRUD operations on user, creating dto for user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,11 +4,11 @@ import { JwtGuard } from 'src/auth/guard';
 import { GetUser } from 'src/auth/decorator';
 import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { EditUserDto } from './dto';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async getUser(req: any) {
-    const user = await this.prisma.user.findUnique({
+  getUsers() {
+    return this.prisma.user.findMany();
+  }
+
+  getUserById(userId: number) {
+    return this.prisma.user.findUnique({
       where: {
-        id: req.sub,
+        id: userId,
       },
     });
+  }
+
+  async updateUserById(userId: number, dto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+
     delete user.passwordHash;
+
     return user;
+  }
+
+  async deleteUserById(userId: number) {
+    await this.prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
   }
 }
