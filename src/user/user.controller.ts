b16dc2ad984c0dd7e3bd5072eb1fa42ca0c2f04d
Nirrax <Nirraxx@gmail.com>
2024-02-21 19:30:53 +0100

    Implementing crud endpoints for user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtGuard } from 'src/auth/guard';
 import { GetUser } from 'src/auth/decorator';
 import { User } from '@prisma/client';
+import { EditUserDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('users')
@@ -12,5 +20,15 @@ export class UserController {
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch('me')
+  updateMe(@GetUser() user: User, @Body() dto: EditUserDto) {
+    return this.userService.updateUserById(user.id, dto);
+  }
+
+  @Delete('me')
+  deleteMe(@GetUser() user: User) {
+    return this.userService.deleteUserById(user.id);
   }
 }
