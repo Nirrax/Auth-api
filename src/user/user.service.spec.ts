9c978fb82fb7d781f909906056456792a2074124
Nirrax <nirraxx@gmail.com>
2025-04-29 14:17:16 +0200

    test: Create unit test setup for userService

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('UserService', () => {
+  let userService: UserService;
+
+  const mockPrismaService = {
+    save: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    delete: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(userService).toBeDefined();
+  });
+});
